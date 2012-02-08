b0adb75a0b55d8ef6c3af6a1a0726dfce3eb097b
Stefan van der Walt <stefan@sun.ac.za>
2012-02-08 02:09:33 -0800

    ENH: Promote as_windows to a utility function.

--- skimage/util/__init__.py
+++ skimage/util/__init__.py
@@ -1,2 +1,2 @@
 from .dtype import *
-
+from .shape import *
---
+++ skimage/util/tests/test_shape.py
@@ -0,0 +1,17 @@
+import numpy as np
+from numpy.testing import assert_equal
+from skimage.util import as_windows
+
+def test_as_windows():
+    X = np.arange(100).reshape((10, 10))
+    W = as_windows(X, win_size=7)
+    assert_equal(W.shape[:2], (4, 4))
+
+    W = as_windows(X, win_size=3)
+    assert_equal(W[0, 0], [[0, 1, 2],
+                           [10, 11, 12],
+                           [20, 21, 22]])
+
+
+if __name__ == '__main__':
+    np.testing.run_module_suite()
