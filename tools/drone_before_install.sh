ba4a4522df8bfa8f50aadd8e07f6eee05d014052
Stefan van der Walt <stefanv@berkeley.edu>
2015-05-19 18:50:45 -0700

    Add drone setup

---
+++ tools/drone_before_install.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+set -ex
+
+export WHEELHOUSE="--no-index --find-links=http://travis-wheels.scikit-image.org/"
+export PIP_DEFAULT_TIMEOUT=60
+sudo service xvfb start
+export DISPLAY=:99.0
+export PYTHONWARNINGS="all"
+export TEST_ARGS="--exe --ignore-files=^_test -v --with-doctest --ignore-files=^setup.py$"
+
+
+retry () {
+    # https://gist.github.com/fungusakafungus/1026804
+    local retry_max=3
+    local count=$retry_max
+    while [ $count -gt 0 ]; do
+        "$@" && break
+        count=$(($count - 1))
+        sleep 1
+    done
+
+    [ $count -eq 0 ] && {
+        echo "Retry failed [$retry_max]: $@" >&2
+        return 1
+    }
+    return 0
+}
+
+sudo apt-get install python-numpy python-scipy python-networkx cython \
+                     python-six python-pil
+
+virtualenv -p python --system-site-packages ~/venv
+
+source ~/venv/bin/activate
+retry pip install wheel flake8 coveralls nose
+
+# install system tk for matplotlib
+sudo apt-get install python-tk
+
+retry pip install $WHEELHOUSE -r requirements.txt
+
+# clean up disk space
+sudo apt-get clean
+sudo rm -rf /tmp/*
+
+export retry
+
+set +ex
+
