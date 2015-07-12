535dff20b187e828469fb4d212d466c26b39e74f
Stefan van der Walt <stefanv@berkeley.edu>
2015-07-12 12:29:33 -0500

    Use virtualenv for running test suite

--- tools/drone_script.sh
+++ tools/drone_script.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 set -ex
+source ~/venv/bin/activate
+
+./tools/build_versions.py
 
 python -c 'import numpy; print("numpy is:", numpy.__version__)'
+
 pip install -e .
 nosetests
