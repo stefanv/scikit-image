92021d733c75d6a528fc2d6c99a4e5e64d6a282c
Stefan van der Walt <stefanv@berkeley.edu>
2015-07-12 11:52:51 -0500

    Print debug info on pip/setuptools

--- tools/drone_before_install.sh
+++ tools/drone_before_install.sh
@@ -34,9 +34,9 @@ virtualenv -p python --system-site-packages ~/venv
 
 source ~/venv/bin/activate
 retry pip install --upgrade setuptools pip
+python -c 'import setuptools; print("Setuptools version:", setuptools.__version__)'
 retry pip install wheel flake8 coveralls nose
 retry pip install $WHEELHOUSE -r requirements.txt
-python -c 'import setuptools; print("Setuptools version:", setuptools.__version__)'
 # clean up disk space
 sudo apt-get clean
 sudo rm -rf /tmp/*
